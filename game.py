933103a4b72ab0684757339209238ea00f918754
MoarDm <moiseyenkoa@gmail.com>
2014-12-06 14:52:49 +0600

    Черви

--- game.py
+++ game.py
@@ -2,6 +2,7 @@
 # coding=UTF-8
 
 from framework import *
+import math, random
 
 @ScreenClass('STARTUP')
 class StartupScreen(AppScreen):
@@ -66,26 +67,82 @@ class GameScreen(AppScreen):
 			self.camera.focus_x += 1
 		if key == KEY.LEFT:
 			self.camera.focus_x -= 1
-			
-class GravityBox(SpriteGameEntity):
-	boxes = []
-	def __init__(self, sx, sy):
-		SpriteGameEntity.__init__(self, "rc/32x32fgA.png")
+#один класс на двух червей
+class Worm(SpriteGameEntity):
+	def __init__(self,sx,sy,sprite,id):
+		SpriteGameEntity.__init__(self,sprite)
+		self.id = id
 		self.x = sx
 		self.y = sy
-		GravityBox.boxes.append(self)
+		self.vx = 1
+		self.vy = 0
+		self.lastTurn = 0
+		self.angle = 0
+		self.angVelocity = 0
+		#!!!подумать как ставить радиус
+		self.radius = 1
+
 	def update(self, dt):
-		dt = dt*20
-		vx = 0
-		vy = 0
-		for ent in GravityBox.boxes:
-			if ent == self:
+		lastTurn += dt
+		if lastTurn > 1000:
+			self.turn()
+			lastTurn = 0
+		for ent in self.game.entities:
+			#проверка на себя и невидимых сущностей
+			if ent == self or ent.sprite.visiable:
 				continue
-			dx = ent.x - self.x
-			dy = ent.y - self.y
-			k = 1.0 / math.sqrt(dx*dx + dy*dy)
-			vx += k*dx*dt
-			vy += k*dy*dt
-		self.x += vx
-		self.y += vy
-		self.end_update_coordinates
+			#проверка столкновения
+			if self.distance(ent) < 10:
+				self.collide(ent)
+
+	def velocity(self):
+		return math.sqrt(self.vx*self.vx + self.vy*self.vY)
+
+	def setVelocity(slef, k = 1):
+		self.vx = k*self.vx
+		self.vy = k*self.vy
+
+	def turn(self, angle = 0):
+		if angle <= 0:
+			angle = random.randint(-15,15)
+			angVelocity = random.randint(0,angle)
+			angleRad = angle / 180.0 * math.pi
+			angVelocityRad = angVelocity / 180.0 * math.pi
+		self.vx, self.vy =\
+		self.vx * math.cos(angVelocityRad) - self.vy * math.sin(angVelocityRad),\
+		self.vx * math.sin(angVelocityRad) + self.vy * math.cos(angVelocityRad)
+		angle -= angVelocity
+		#self.sprite.rotate(angVelocity)
+ 
+
+
+	#возвращает расстояние от текущей сущности до указанной
+	def distance(self, entity):
+		dx = ent.x - self.x
+		dy = ent.y - self.y
+		return math.sqrt(dx*dx + dy*dy)-entity.radius
+
+	#действие столкновения
+	def collide(self, entity):
+		if entity.a.__class__.__name__ == 'Apple':
+			#ent.eaten method(self)
+			self.eat()
+		elif entity.a.__class__.__name__ == 'Worm':
+			dx = abs(entity.x - self.x)
+			dy = abs(entity.y - self.y)
+			dl = math.sqrt(dx*dx + dy*dy)
+			dx /= dl
+			dy /= dl
+			#скалярное проиведение
+			scalarMul = self.vx*ent.vx + self.vy*ent.vy
+			self.vx = self.vx - 2 * dx * scalarMul
+			self.vy = self.vy - 2 * dy * scalarMul
+			ent.vx = ent.vx - 2 * dx * scalarMul
+			ent.vy = ent.vy - 2 * dy * scalarMul
+			self.lastTurn = 0
+			ent.lastTurn = 0
+
+	def eat(self):
+		#анимка, звук поедания яблока или надо будет переопределить для каждого червя
+		pass
+
